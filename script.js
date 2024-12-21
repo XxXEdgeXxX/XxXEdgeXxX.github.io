20c14c88bb1dfe959b9ee7432e9e1063b02b34de
XxXEdgeXxX <noahbuch@hotmail.com>
2024-12-21 14:34:00 +0100

    removed commas in galaxy gals

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var Version = "1.0.7";
+    var Version = "1.0.8";
 
     const formSelect = document.getElementById('formSelect');
     const nameSelect = document.getElementById('nameSelect');
@@ -9,72 +9,74 @@ document.addEventListener('DOMContentLoaded', () => {
     const selectedList = document.getElementById('selectedList');
     const dataSummary = document.getElementById('dataSummary');
     const unitselector = document.getElementById('UnitSelector');
+    const SearchBar = document.getElementById('searchInput');
+    const SearchResults = document.getElementById('searchResults');
     console.log("Current version: " + Version);
 
     const clearListButton = document.createElement('button');
     clearListButton.textContent = 'Clear List';
     clearListButton.addEventListener('click', clearSelectedList);
-    dataSummary.parentNode.insertBefore(clearListButton, dataSummary.nextSibling); // Insert before dataSummary
+    dataSummary.parentNode.insertBefore(clearListButton, dataSummary.nextSibling);
 
     function clearSelectedList() {
-        selectedItems.length = 0; // Clear the array
-        selectedList.innerHTML = ''; // Clear the list display
-        updateDataSummary(); // Update the summary
+        selectedItems.length = 0;
+        selectedList.innerHTML = '';
+        updateDataSummary();
     }
-    
-  
+
+
 
     var coll = document.getElementsByClassName("collapsible");
-          for (var i = 0; i < coll.length; i++) {
-            coll[i].addEventListener("click", function() {
-              this.classList.toggle("active");
-              var content = this.nextElementSibling;
-              if (content.style.display === "block") {
+    for (var i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
                 content.style.display = "none";
-              } else {
+            } else {
                 content.style.display = "block";
-              }
-            });
-            coll[i].nextElementSibling.style.display = "none";
-          }
-    const Unitdata = []; // Initialize an empty array to store CSV data
+            }
+        });
+        coll[i].nextElementSibling.style.display = "none";
+    }
+    const Unitdata = [];
     const SmallUnitImages = [];
-    const selectedItems = []; // Array to store selected items
+    const selectedItems = [];
 
     const items = [
-        { color: "Rarity",       index: 4,  category: undefined },
-        { color: "Acquired by",  index: 5,  category: undefined },
-        { color: "XP",           index: 35, category: "other" },
-        { color: "Red",          index: 6,  category: "catfruit" },
-        { color: "Purple",       index: 7,  category: "catfruit" },
-        { color: "Blue",         index: 8,  category: "catfruit" },
-        { color: "Green",        index: 9,  category: "catfruit" },
-        { color: "Yellow",       index: 10, category: "catfruit" },
-        { color: "Ancient",      index: 11, category: "catfruit" },
-        { color: "Gold",         index: 12, category: "catfruit" },
-        { color: "Aku",          index: 13, category: "catfruit" },
-        { color: "Epic",         index: 14, category: "catfruit" },
-        { color: "Red",          index: 15, category: "seeds" },
-        { color: "Green",        index: 16, category: "seeds" },
-        { color: "Purple",       index: 17, category: "seeds" },
-        { color: "Blue",         index: 18, category: "seeds" },
-        { color: "Yellow",       index: 19, category: "seeds" },
-        { color: "Ancient",      index: 20, category: "seeds" },
-        { color: "Gold",         index: 21, category: "seeds" },
-        { color: "Aku",          index: 22, category: "seeds" },
-        { color: "Epic",         index: 23, category: "seeds" },
-        { color: "Purple",       index: 24, category: "stones" },
-        { color: "Red",          index: 25, category: "stones" },
-        { color: "Blue",         index: 26, category: "stones" },
-        { color: "Green",        index: 27, category: "stones" },
-        { color: "Yellow",       index: 28, category: "stones" },
-        { color: "Epic",         index: 29, category: "stones" },
-        { color: "Purple",       index: 30, category: "gems" },
-        { color: "Blue",         index: 31, category: "gems" },
-        { color: "Red",          index: 32, category: "gems" },
-        { color: "Green",        index: 33, category: "gems" },
-        { color: "Yellow",       index: 34, category: "gems" },
-        
+        { color: "Rarity", index: 4, category: undefined },
+        { color: "Acquired by", index: 5, category: undefined },
+        { color: "XP", index: 35, category: "other" },
+        { color: "Red", index: 6, category: "catfruit" },
+        { color: "Purple", index: 7, category: "catfruit" },
+        { color: "Blue", index: 8, category: "catfruit" },
+        { color: "Green", index: 9, category: "catfruit" },
+        { color: "Yellow", index: 10, category: "catfruit" },
+        { color: "Ancient", index: 11, category: "catfruit" },
+        { color: "Gold", index: 12, category: "catfruit" },
+        { color: "Aku", index: 13, category: "catfruit" },
+        { color: "Epic", index: 14, category: "catfruit" },
+        { color: "Red", index: 15, category: "seeds" },
+        { color: "Green", index: 16, category: "seeds" },
+        { color: "Purple", index: 17, category: "seeds" },
+        { color: "Blue", index: 18, category: "seeds" },
+        { color: "Yellow", index: 19, category: "seeds" },
+        { color: "Ancient", index: 20, category: "seeds" },
+        { color: "Gold", index: 21, category: "seeds" },
+        { color: "Aku", index: 22, category: "seeds" },
+        { color: "Epic", index: 23, category: "seeds" },
+        { color: "Purple", index: 24, category: "stones" },
+        { color: "Red", index: 25, category: "stones" },
+        { color: "Blue", index: 26, category: "stones" },
+        { color: "Green", index: 27, category: "stones" },
+        { color: "Yellow", index: 28, category: "stones" },
+        { color: "Epic", index: 29, category: "stones" },
+        { color: "Purple", index: 30, category: "gems" },
+        { color: "Blue", index: 31, category: "gems" },
+        { color: "Red", index: 32, category: "gems" },
+        { color: "Green", index: 33, category: "gems" },
+        { color: "Yellow", index: 34, category: "gems" },
+
     ];
 
 
@@ -85,34 +87,34 @@ document.addEventListener('DOMContentLoaded', () => {
             for (let row of rows) {
                 Unitdata.push(row.split(','));
             }
-            updateNameOptions(); // Initialize the name dropdown
+            updateNameOptions();
         });
 
-        function loadUnitSelectionImages() {
-            fetch('BCDATA/mini_units.csv')
-              .then(response => response.text())
-              .then(text => {
+    function loadUnitSelectionImages() {
+        fetch('BCDATA/mini_units.csv')
+            .then(response => response.text())
+            .then(text => {
                 const rows = text.split('\n');
-                const imageLinks = rows.filter(row => !row.startsWith('SUMMON')) // Filter out "SUMMON" entries
-                  .map(row => row.split(',')[0]); // Extract links from the first column
-       
-                // Update unitselector elements with image links
+                const imageLinks = rows.filter(row => !row.startsWith('SUMMON'))
+                    .map(row => row.split(',')[0]);
+
+
                 const unitOptions = unitselector.querySelectorAll('option');
                 unitOptions.forEach((option, index) => {
-                  if (imageLinks[index]) {
-                    option.style.backgroundImage = `url(${imageLinks[index]})`;
-                    option.style.backgroundSize = 'cover';
-                    option.style.backgroundPosition = 'center';
-                  } else {
-                    // Set a default background or remove existing background if no link found
-                    option.style.backgroundImage = 'none';
-                  }
+                    if (imageLinks[index]) {
+                        option.style.backgroundImage = `url(${imageLinks[index]})`;
+                        option.style.backgroundSize = 'cover';
+                        option.style.backgroundPosition = 'center';
+                    } else {
+
+                        option.style.backgroundImage = 'none';
+                    }
                 });
-              })
-              .catch(error => {
+            })
+            .catch(error => {
                 console.error('Error loading mini_units.csv:', error);
-              });
-          }
+            });
+    }
     loadUnitSelectionImages();
     formSelect.addEventListener('change', updateNameOptions);
     nameSelect.addEventListener('change', () => {
@@ -123,7 +125,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function loadUnitImages(unitIndex) {
         const jsonPath = 'BCDATA/unit_images.json';
         console.log(`Fetching JSON from: ${jsonPath}`);
-        
+
         fetch(jsonPath)
             .then(response => {
                 if (!response.ok) {
@@ -133,15 +135,15 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             .then(imageData => {
                 const unitImages = imageData[unitIndex - 2]; // Adjust index as needed
-                
+
                 console.log('Image data loaded:', unitImages);
 
-                tabList.innerHTML = ''; // Clear existing tabs
-                imageDisplay.innerHTML = ''; // Clear existing image display
+                tabList.innerHTML = '';
+                imageDisplay.innerHTML = '';
 
-                // Create tabs and load images
+
                 unitImages.forEach(({ Form, Link }) => {
-                    if (Link) { // Only create tabs for available forms
+                    if (Link) {
                         const tab = document.createElement('li');
                         tab.textContent = `Form ${Form}`;
                         tab.addEventListener('click', () => displayImage(Link));
@@ -149,7 +151,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     }
                 });
 
-                // Display the first image by default
+
                 if (unitImages[0] && unitImages[0].Link) {
                     displayImage(unitImages[0].Link);
                 }
@@ -161,46 +163,46 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function updateNameOptions() {
         const formType = formSelect.value;
-        nameSelect.innerHTML = ''; // Clear existing options
+        nameSelect.innerHTML = '';
 
         for (let row of Unitdata.slice(2)) {
             let optionText = '';
             let optionValue = Unitdata.indexOf(row);
             if (formType === 'number') {
-                optionText = row[0]; // Use the Number field
+                optionText = row[0];
             } else {
                 const formIndex = formType === 'normal' ? 1 : formType === 'evolved' ? 2 : 3;
-                optionText = row[formIndex]; // Use the form name field
+                optionText = row[formIndex];
             }
             if (optionText) {
                 const option = document.createElement('option');
-                option.value = optionValue; // Use the actual index of the row
+                option.value = optionValue;
                 option.textContent = optionText;
                 nameSelect.appendChild(option);
             }
         }
-        displayData(); // Update displayed data
+        displayData();
     }
 
     function displayData() {
         const selectedIndex = nameSelect.value;
         const rowData = Unitdata[selectedIndex];
-        dataDisplay.innerHTML = ''; // Clear existing data display
-    
+        dataDisplay.innerHTML = '';
+
         const renderItem = (item, dataValue, imagePath) => {
             const text = `${item.color} ${item.category != undefined && item.category != "other" ? item.category : ""}: ${dataValue}`;
             const p = document.createElement('p');
             p.textContent = text;
-    
+
             if (item.category) {
                 const img = document.createElement('img');
                 img.src = imagePath;
                 p.appendChild(img);
             }
-    
+
             return p;
         };
-    
+
         items.forEach(item => {
             const dataValue = rowData[item.index];
             if (dataValue) {
@@ -208,13 +210,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 dataDisplay.appendChild(renderItem(item, dataValue, imagePath));
             }
         });
-    
-        // Create foldout menu for empty data items
+
+
         const detailsElement = document.createElement('details');
         const summaryElement = document.createElement('summary');
         summaryElement.textContent = 'See less relevant data';
         detailsElement.appendChild(summaryElement);
-    
+
         items.forEach(item => {
             const dataValue = rowData[item.index];
             if (!dataValue) {
@@ -222,39 +224,39 @@ document.addEventListener('DOMContentLoaded', () => {
                 detailsElement.appendChild(renderItem(item, dataValue, imagePath));
             }
         });
-    
+
         dataDisplay.appendChild(detailsElement);
-    
-        // Add button to add current selection to the list
+
+
         const addButton = document.createElement('button');
         addButton.textContent = 'Add to List';
         addButton.addEventListener('click', () => addToSelectedList(selectedIndex));
         dataDisplay.appendChild(addButton);
     }
-    
+
 
     function addToSelectedList(index) {
         const item = Unitdata[index];
         selectedItems.push(item);
 
         const li = document.createElement('li');
-        li.textContent = item[3]; // Display the name or identifier of the item
+        li.textContent = item[3];
 
-        // Add click event listener to the list item
+
         li.addEventListener('click', () => {
-            // Find the index of the clicked item in the original data array
+
             const clickedIndex = Unitdata.indexOf(item);
             if (clickedIndex !== -1) {
-                nameSelect.value = clickedIndex; // Set the nameSelect dropdown
-                displayData(); // Display the data for the clicked item
-                loadUnitImages(clickedIndex); //Load the images for the clicked item
+                nameSelect.value = clickedIndex;
+                displayData();
+                loadUnitImages(clickedIndex);
             }
         });
 
         const removeButton = document.createElement('button');
         removeButton.textContent = 'Remove';
         removeButton.addEventListener('click', (event) => {
-            event.stopPropagation(); // Prevent the list item click event from firing
+            event.stopPropagation();
             selectedItems.splice(selectedItems.indexOf(item), 1);
             selectedList.removeChild(li);
             updateDataSummary();
@@ -266,8 +268,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function updateDataSummary() {
-        dataSummary.innerHTML = ''; // Clear existing summary
-    
+        dataSummary.innerHTML = '';
+
         const summary = {};
         selectedItems.forEach(item => {
             items.forEach(itemData => {
@@ -281,25 +283,25 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             });
         });
-    
+
         for (const [key, value] of Object.entries(summary)) {
             const p = document.createElement('p');
             p.textContent = `${key}: ${value}`;
-    
-            // Add image to the summary
-            const parts = key.match(/(.*) \((.*)\)/); // Extract color and category
+
+
+            const parts = key.match(/(.*) \((.*)\)/);
             if (parts) {
-              const color = parts[1];
-              const category = parts[2];
-              let imgSrc;
-              if (category === 'Other') {
-                  imgSrc = `BCDATA/materials/No_${color}.png`
-              } else {
-                  imgSrc = `BCDATA/materials/${category}/${color}.png`;
-              }
+                const color = parts[1];
+                const category = parts[2];
+                let imgSrc;
+                if (category === 'Other') {
+                    imgSrc = `BCDATA/materials/No_${color}.png`
+                } else {
+                    imgSrc = `BCDATA/materials/${category}/${color}.png`;
+                }
                 const img = document.createElement('img');
                 img.src = imgSrc;
-                img.style.width = "20px"; //added styling to the image to not be too large
+                img.style.width = "20px";
                 img.style.height = "20px";
                 img.style.marginLeft = "5px";
                 p.appendChild(img);
@@ -307,9 +309,27 @@ document.addEventListener('DOMContentLoaded', () => {
             dataSummary.appendChild(p);
         }
     }
-        
-        function displayImage(link) {
-            imageDisplay.innerHTML = `<img src="${link}" alt="Unit Image">`;
-        }
-    });
 
+    function displayImage(link) {
+        imageDisplay.innerHTML = `<img src="${link}" alt="Unit Image">`;
+    }
+
+    function Search()
+    {
+        let input = SearchBar.value;
+        input = input.toLowerCase();
+        const results = Unitdata.filter(row => 
+            row.slice(0, 4).some(cell => cell.toLowerCase().includes(input))
+        );
+
+        const resultList = document.createElement('ul');
+        results.forEach(result => {
+            const li = document.createElement('li');
+            li.textContent = result.slice(0, 4).join(', ');
+            resultList.appendChild(li);
+        });
+
+        SearchResults.innerHTML = '';
+        SearchResults.appendChild(resultList);
+    }
+});
